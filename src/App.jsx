3257d78665be72ac741de3f75b03b28747ccd6bc
Ashokdev10 <imashokdev@gmail.com>
2021-01-02 17:15:09 +0530

    ForEach added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
+import ArrayMap from "./app/MapFunc";
 
 const App = () => {
-
-  function MapFunc() {
-    const a1 = [1, 2, 3, 4];
-    const newArr = a1.map((el) => {
-      return el * 2;
-    });
-
-    console.log("OLD ARRAY: ", a1);
-    console.log("NEW ARRAY: ", newArr);
-  }
-
-  useEffect(() => {
-    MapFunc();
-  }, []);
-
   return (
     <div>
       <h1>React App</h1>
+      <ArrayMap />
     </div>
   );
 };
---
+++ src/app/MapFunc.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+
+const ArrayMap = () => {
+  function forEachFunc() {
+    const a1 = [1, 2, 3, 4];
+    const newArr = a1
+      .map((el) => {
+        return el * 2;
+      })
+      .filter((el) => el < 4);
+
+    console.log("OLD ARRAY: ", a1);
+    console.log("NEW ARRAY: ", newArr);
+  }
+
+  function MapFunc() {
+    const a1 = [1, 2, 3, 4];
+    const newArr = a1
+      .map((el) => {
+        return el * 2;
+      })
+      .filter((el) => el > 6);
+
+    console.log("MAP OLD ARRAY: ", a1);
+    console.log("MAP NEW ARRAY: ", newArr);
+  }
+
+  useEffect(() => {
+    forEachFunc();
+    MapFunc();
+  }, []);
+
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  );
+};
+
+export default ArrayMap;
